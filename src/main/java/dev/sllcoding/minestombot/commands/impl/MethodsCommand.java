270961204611d3c0dc890e015fc1771359bb1a2e
Luis <luisjk266@gmail.com>
2021-03-31 21:38:32 +0100

    Disabled JavadocCommand and added MethodsCommand. It kinda fills the chat so I'm still considering pagination.

--- src/main/java/dev/sllcoding/minestombot/commands/impl/MethodsCommand.java
+++ src/main/java/dev/sllcoding/minestombot/commands/impl/MethodsCommand.java
@@ -1,6 +1,7 @@
 package dev.sllcoding.minestombot.commands.impl;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import dev.sllcoding.minestombot.commands.Command;
 import dev.sllcoding.minestombot.documentation.GitHubManager;
 import dev.sllcoding.minestombot.documentation.JavadocParser;
 import dev.sllcoding.minestombot.utils.EmbedUtils;
@@ -13,13 +14,13 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import java.util.Comparator;
 import java.util.List;
 
-public class JavadocCommand /*extends Command*/ {
+public class MethodsCommand extends Command {
 
-    public JavadocCommand() {
-        //super("javadoc", "Search the Minestom Javadoc", "Utilities");
+    public MethodsCommand() {
+        super("methods", "See the methods inside a class", "Utilities");
     }
 
-    //@Override
+    @Override
     public void onCommand(Member member, Message message) {
         String[] args = message.getContentRaw().split(" ");
         if (args.length > 1) {
@@ -34,6 +35,18 @@ public class JavadocCommand /*extends Command*/ {
                                 ClassOrInterfaceDeclaration clazz = results.get(0);
                                 EmbedBuilder embedBuilder = EmbedUtils.getDefaultEmbed();
                                 embedBuilder.setTitle(clazz.getNameAsString(), "https://minestom.github.io/Minestom/index-all.html#I:" + clazz.getNameAsString().split("")[0].toUpperCase());
+                                StringBuilder builder = new StringBuilder();
+                                clazz.getMethods().forEach(method -> {
+                                    if (method.isAnnotationPresent(Deprecated.class)) {
+                                        builder.append("**~~").append(method.getNameAsString()).append("~~** - ");
+                                    } else {
+                                        builder.append("**").append(method.getNameAsString()).append("** - ");
+                                    }
+                                    builder.append(method.getType().asString()).append("\n");
+                                });
+                                for (String string : justify(builder.toString(), 1024)) {
+                                    embedBuilder.addField("", string, true);
+                                }
                                 replyAgain.editMessage(embedBuilder.build()).queue();
                             } else {
                                 replyAgain.editMessage(EmbedUtils.getNoResultsEmbed(args[1]).build()).queue();
@@ -51,6 +64,21 @@ public class JavadocCommand /*extends Command*/ {
         } else {
             message.reply(EmbedUtils.getInvalidArgumentsEmbed().build()).queue();
         }
+    }
+
+    private String[] justify(String s, int limit) {
+        StringBuilder justifiedText = new StringBuilder();
+        StringBuilder justifiedLine = new StringBuilder();
+        String[] words = s.split("\n");
+        for (int i = 0; i < words.length; i++) {
+            justifiedLine.append(words[i]).append("\n ");
+            if (i+1 == words.length || justifiedLine.length() + words[i+1].length() > limit) {
+                justifiedLine.deleteCharAt(justifiedLine.length() - 1);
+                justifiedText.append(justifiedLine.toString()).append("jufidslhfusjdifld");
+                justifiedLine = new StringBuilder();
+            }
+        }
+        return justifiedText.toString().split("jufidslhfusjdifld");
     }
 
 }
