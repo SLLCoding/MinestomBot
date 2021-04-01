702a9623128fcac5bc6c2679fca47012194f8246
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 18:15:02 -0400

    Changed Capitalization of ping to Ping and added minestom logo

--- src/main/java/dev/sllcoding/minestombot/commands/impl/Ping.java
+++ src/main/java/dev/sllcoding/minestombot/commands/impl/Ping.java
@@ -11,18 +11,18 @@ public class Ping extends Command {
     EmbedBuilder eb = new EmbedBuilder();
 
     public Ping() {
-        super("ping", "Gets bot ping", "Utilities");
+        super("Ping", "Gets bot ping", "Utilities");
     }
 
     @Override
     public void onCommand(Member member, Message message) {
         long time = System.currentTimeMillis();
-        eb.setTitle("Pong!");
+        eb.setTitle("Pong!", "https://gblobscdn.gitbook.com/spaces%2F-MJpMt9ABrjwz4C2P2ZK%2Favatar-1602920266287.png");
         eb.setDescription("Getting ping.");
         eb.setFooter(member.getEffectiveName(), member.getUser().getAvatarUrl());
         eb.setColor(Color.RED);
         message.getChannel().sendMessage(eb.build()).queue(response -> {
-            eb.setTitle("Pong Complete!");
+            eb.setTitle("Pong Complete!", "https://gblobscdn.gitbook.com/spaces%2F-MJpMt9ABrjwz4C2P2ZK%2Favatar-1602920266287.png");
             eb.setDescription("Pong: " + (System.currentTimeMillis() - time) + " ms");
             eb.setColor(Color.GREEN);
             response.editMessage(eb.build()).queue();
