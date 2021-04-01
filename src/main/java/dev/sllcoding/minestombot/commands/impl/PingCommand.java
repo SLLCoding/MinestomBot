11973c6ecf90b974e4dc556032777ab7131c2a0a
Luis <39528861+SLLCoding@users.noreply.github.com>
2021-04-01 23:30:00 +0100

    Update and rename Ping.java to PingCommand.java

--- src/main/java/dev/sllcoding/minestombot/commands/impl/PingCommand.java
+++ src/main/java/dev/sllcoding/minestombot/commands/impl/PingCommand.java
@@ -22,8 +22,9 @@ public class Ping extends Command {
         eb.setFooter(member.getEffectiveName(), member.getUser().getAvatarUrl());
         eb.setColor(Color.YELLOW);
         message.getChannel().sendMessage(eb.build()).queue(response -> {
+            long ping = System.currentTimeMillis() - time;
             eb.setAuthor("Pong Complete!", "https://minestom.github.io/Minestom/", "https://gblobscdn.gitbook.com/spaces%2F-MJpMt9ABrjwz4C2P2ZK%2Favatar-1602920266287.png");
-            eb.setDescription("Pong: " + (System.currentTimeMillis() - time) + " ms");
+            eb.setDescription("Pong: " + ping + " ms");
             eb.setColor(Color.GREEN);
             response.editMessage(eb.build()).queue();
         });
