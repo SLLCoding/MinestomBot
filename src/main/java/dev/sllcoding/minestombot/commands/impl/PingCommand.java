1852b4e6e4a31a24eefcc571a86a41b75833d8ac
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 19:54:13 -0400

    change ping calculations

--- src/main/java/dev/sllcoding/minestombot/commands/impl/PingCommand.java
+++ src/main/java/dev/sllcoding/minestombot/commands/impl/PingCommand.java
@@ -7,10 +7,10 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 
-public class Ping extends Command {
+public class PingCommand extends Command {
     EmbedBuilder eb = new EmbedBuilder();
 
-    public Ping() {
+    public PingCommand() {
         super("Ping", "Gets bot ping", "Utilities");
     }
 
@@ -23,8 +23,8 @@ public class Ping extends Command {
         eb.setColor(Color.YELLOW);
         message.getChannel().sendMessage(eb.build()).queue(response -> {
             long ping = System.currentTimeMillis() - time;
-            eb.setAuthor("Pong Complete!", "https://minestom.github.io/Minestom/", "https://gblobscdn.gitbook.com/spaces%2F-MJpMt9ABrjwz4C2P2ZK%2Favatar-1602920266287.png");
             eb.setDescription("Pong: " + ping + " ms");
+            eb.setAuthor("Pong Complete!", "https://minestom.github.io/Minestom/", "https://gblobscdn.gitbook.com/spaces%2F-MJpMt9ABrjwz4C2P2ZK%2Favatar-1602920266287.png");
             eb.setColor(Color.GREEN);
             response.editMessage(eb.build()).queue();
         });
